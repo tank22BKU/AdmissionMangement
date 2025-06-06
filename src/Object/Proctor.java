5c451cc7c74b0ab3ca6a4fcbae2dea838976ce6f
DANG VAN TAN <161315905+tank22BKU@users.noreply.github.com>
2025-06-06 16:46:15 +0700

    Add : Additional print method

--- src/Object/Proctor.java
+++ src/Object/Proctor.java
@@ -15,4 +15,16 @@ public class Proctor extends Person {
     public void setWorkPlace(String workPlace) {
         this.workPlace = workPlace;
     }
+
+    public void printInf(){
+        System.out.println("\n***************************************************");
+        System.out.println("****       ~~ Proctor Information ~~            ***");
+        System.out.println("***************************************************\n");
+        System.out.println("*********Proctor ID: " + this.getId());
+        System.out.println("*********Full name : " + this.getName());
+        System.out.println("*********Gender : " + (this.getGender() ? "FEMALE" : "MALE"));
+        System.out.println("*********Year of birth : " + this.getYearOfBirth());
+        System.out.println("*********Home town : " + this.workPlace);
+        System.out.println("******************************************************\n");
+    }
 }
